d13963b644affcb2051270b700a3ca4267573027
Kami Joseph <josephkiarie561@gmail.com>
2024-12-10 13:54:29 +0300

    Linear Search

--- 10_linear_search.py
+++ 10_linear_search.py
@@ -17,3 +17,20 @@ else:
     print(f"Target value is at index, {result}")
     
 #The time complexity of the above algorithm is O(n).
+
+# Code 2
+def linear_search(arr, target):
+    for index, element in enumerate(arr):
+        if element == target:
+            return index
+    return -1
+
+arr = [3,4,6,10,30,40]
+target = 6
+
+results = linear_search(arr, target)
+
+if result != -1:
+    print(f"Element {target} found at index {results}.")
+else:
+    print(f"Element {target} was not found.")
