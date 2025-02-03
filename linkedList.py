7cf1ecfd8f44ea4e13b713bfb34209ef685cdc84
Kami Joseph <josephkiarie561@gmail.com>
2025-02-03 09:24:39 +0300

    iters

--- linkedList.py
+++ linkedList.py
@@ -11,3 +11,13 @@ class LinkedList:
     def insertBegining(self, data):
         node = Node(data, self.head)
         self.head = node
+        
+    def print(self):
+        if self.head is None:
+            print("Linked List Empty")
+            return
+        
+        iter = self.head
+        while iter:
+            llString += str(iter.data) + "-->"
+            iter = iter.next
