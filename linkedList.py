b0eacf0596855dc0fb62bc2e0c6f48c7c0afa963
Kami Joseph <josephkiarie561@gmail.com>
2025-02-03 09:17:42 +0300

    insert at the begining method

--- linkedList.py
+++ linkedList.py
@@ -3,3 +3,11 @@ class Node:
     def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        
+    def insertBegining(self, data):
+        node = Node(data, self.head)
+        self.head = node
